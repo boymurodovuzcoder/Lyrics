dabe6e3ed04a7c4995356e941e2b500d9c447fda
boymurodov <boymurodovuzcoder@gmail.com>
2020-10-16 01:10:50 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Learn English by Lyrics (lebl)
+
+This App is very useful for those who want to learn english through listening to english musics.
+
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+npm run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/pages/Index.vue
@@ -0,0 +1,155 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="container container-fluid">
+      <transition name="bounce">
+        <div id="detail" v-if="detail">
+            <div class="form-group area">
+            <label for="exampleFormControlTextarea1">Enter lyrics:</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" v-model="lyrics"></textarea>
+        </div>
+        <div class="form-group row area">
+          <div class="col">
+              <label for="upload">Upload Music</label>
+              <input type="file" class="form-control-file" id="upload" @change="handleFiles">
+          </div>
+          <div class="col">
+            <label for="Number">Number of Omitting Words: </label>
+            <input type="number" class="form-control" id="Number" v-model="omitNumber">
+          </div>
+        </div>
+        <button type="button" @click="startAction()"  class="btn btn-primary btn-lg btn-block">Start!</button>
+      </div>
+      </transition>
+      <button v-if="!detail" type="button" class="btn btn-info" style="margin-top: 1rem;" @click="detail=!detail">Back!</button>
+      <div style="margin-top: 1rem; margin-bottom: 1rem;">
+        <audio id="audio" controls style="width:100%;">
+          <source src="" id="src" />
+        </audio>
+      </div>
+        <transition name="bounce">
+          <div v-if="!detail" style="margin-top: 1rem; margin-bottom: 1rem;">
+          <div>
+            <xmp>
+              <span v-html="formatted"></span>
+            </xmp>
+          </div>
+          <button type="button" @click="endAction()" class="btn btn-primary">Finish!</button>
+          <button type="button" @click="showAnswers()" class="btn btn-warning" style="margin-left: 1rem;">Show Answers</button>
+        </div>
+        </transition>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import $ from 'jquery'
+
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      detail: true,
+      lyrics: null,
+      omitNumber: null,
+      formatted: null,
+      result: null,
+      mp3Url: null
+    }
+  },
+  methods: {
+    handleFiles (event) {
+      var files = event.target.files
+      this.mp3Url = URL.createObjectURL(files[0])
+    },
+    endAction () {
+      for (var i = 0; i < this.result.length; i++) {
+        if (this.result[i].toLocaleLowerCase() === document.getElementById(`${i}`).value.toLocaleLowerCase()) {
+          document.getElementById(`${i}`).style.borderColor = 'green'
+        } else {
+          document.getElementById(`${i}`).style.borderColor = 'red'
+        }
+      }
+    },
+    showAnswers () {
+      for (var i = 0; i < this.result.length; i++) {
+        document.getElementById(`${i}`).value = this.result[i]
+        document.getElementById(`${i}`).style.borderColor = 'green'
+      }
+    },
+    startAction () {
+      if (this.omitNumber && this.lyrics) {
+        this.detail = false
+      }
+      if (this.mp3Url) {
+        $('#src').attr('src', this.mp3Url)
+        document.getElementById('audio').load()
+        document.getElementById('audio').play()
+      }
+      if (!this.lyrics) {
+        alert('Textarea is empty!')
+      } else if (!this.omitNumber) {
+        alert('Number of omitting words is empty!')
+      } else {
+        var r = /(\w|\s)*\w(?=")|\w+('\w+)?/gm
+        var array = this.lyrics.match(r)
+        var copyArray = []
+        array.forEach(word => {
+          if ((word.length > 3) && (word.indexOf("'") === -1)) {
+            copyArray.push(word)
+          }
+        })
+        console.log(copyArray)
+        var result = this.getRandom(copyArray, this.omitNumber)
+        var lyrics = this.lyrics
+        var index = 0
+        result.forEach(word => {
+          lyrics = lyrics.replace(word, `<input type="text" class="rounded" style="width: ${word.length * 10}px; border-color: black; margin:0.1rem;" id="${index}">`)
+          index += 1
+        })
+        this.formatted = lyrics
+        console.log(result)
+        this.result = result
+        console.log(lyrics)
+      }
+    },
+    getRandom (arr, n) {
+      var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len)
+      if (n > len) {
+        throw new RangeError('getRandom: more elements taken than available')
+      }
+      while (n--) {
+        var x = Math.floor(Math.random() * len)
+        result[n] = arr[x in taken ? taken[x] : x]
+        taken[x] = --len in taken ? taken[len] : len
+      }
+      return result
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .area {
+    font-size: 1rem;
+    font-weight: 500;
+  }
+  .bounce-enter-active {
+    animation: bounce-in .5s;
+  }
+  .bounce-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+</style>
